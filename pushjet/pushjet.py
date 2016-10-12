981d8ea5e43fe72a71063dbca29f2476b435d1df
obskyr <powpowd@gmail.com>
2016-10-12 03:57:27 +0200

    Handle public-only Service instances.

--- pushjet/pushjet.py
+++ pushjet/pushjet.py
@@ -3,19 +3,33 @@
 from __future__ import unicode_literals
 
 import requests
+from functools import wraps
 
-API_URL = 'https://api.pushjet.io/'    
+API_URL = 'https://api.pushjet.io/'
+
+def requires_secret(func):
+    @wraps(func)
+    def with_secret_requirement(self, *args, **kwargs):
+        if self.secret is None:
+            raise WriteAccessError("The Service doesn't have a secret "
+                "key provided, and therefore lacks write permission.")
+        func(*args, **kwargs)
+
+    return with_secret_requirement
 
 class Service(object):
     def __init__(self, secret=None, public=None):
         pass
     
+    @requires_secret
     def send(self, message, title=None, link=None, importance=None):
         pass
 
+    @requires_secret
     def update(self, name=None, icon_url=None):
         pass
 
+    @requires_secret
     def delete(self):
         pass
     
